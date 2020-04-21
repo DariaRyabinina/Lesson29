10e9e5dc41318e4c667bf430cf43aaf55ad403df
Daria <ryabinina@i-teco.ru>
2020-04-22 01:07:15 +0300

    init Less29

---
+++ src/test/java/org/DariaRyabinina/steps/Less29.java
@@ -0,0 +1,21 @@
+package org.DariaRyabinina.steps;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.BeforeMethod;
+
+@CucumberOptions(plugin = "json:target/cucumber-report.json",
+        features = "src/test/resources/feature",
+        glue = "org.DariaRyabinina.steps",
+        tags = "@allTest"
+        )
+
+public class Less29 extends AbstractTestNGCucumberTests {
+    @BeforeMethod
+    public void initDriver() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+    }
+}
+
